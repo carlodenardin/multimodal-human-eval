f9ca1c24db44ffe6d1784ef85325c57ef9c2b9e2
Carlo De Nardin <denardincarlo@gmail.com>
2024-11-07 22:19:05 +0100

    Added PaliGemma

--- src/run.py
+++ src/run.py
@@ -1,4 +1,9 @@
-import ollama
+from transformers import AutoProcessor, PaliGemmaForConditionalGeneration
+from PIL import Image
+
+# import ollama
+import requests
+import torch
 import tqdm
 
 from utils.const import PROMPT_INSTRUCTION
@@ -6,6 +11,11 @@ from multimodal_human_eval.data import read_problems, write_jsonl
 
 BEGIN_TRIM = "```python"
 END_TRIM = "```"
+
+model_id = "google/paligemma-3b-mix-224"
+
+model = PaliGemmaForConditionalGeneration.from_pretrained(model_id).eval()
+processor = AutoProcessor.from_pretrained(model_id)
 
 
 def trim_code(text: str):
@@ -20,15 +30,24 @@ def trim_code(text: str):
 
 
 def generate_completion(prompt: str):
-    response = ollama.chat(
-        model="llama3.2",
-        messages=[
-            {
-                "role": "user",
-                "content": PROMPT_INSTRUCTION + prompt,
-            }
-        ],
-    )
+    """response = ollama.chat(
+            model="llama3.2",
+            messages=[
+                    {
+                            "role": "user",
+                            "content": PROMPT_INSTRUCTION + prompt,
+                    }
+            ],
+    )"""
+    model_inputs = processor(text=prompt, return_tensors="pt")
+    input_len = model_inputs["input_ids"].shape[-1]
+
+    with torch.inference_mode():
+        generation = model.generate(
+            **model_inputs, max_new_tokens=100, do_sample=False)
+        generation = generation[0][input_len:]
+        decoded = processor.decode(generation, skip_special_tokens=True)
+        print(decoded)
     return trim_code(response["message"]["content"])
 
 
