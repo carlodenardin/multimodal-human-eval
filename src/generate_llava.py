11560d7b0c422808d0b784a659152cd91b0851dd
Carlo De Nardin <denardincarlo@gmail.com>
2024-12-27 17:19:06 +0100

    Added LlavaGenerator

--- src/generate_llava.py
+++ src/generate_llava.py
@@ -23,6 +23,8 @@ MODELS_NAME = [
 ]
 
 print(torch.cuda.is_available())
+print(torch.cuda.device_count())
+print(torch.cuda.get_device_name(0) if torch.cuda.is_available() else "No GPU")
 
 
 class LlavaCodeGenerator:
